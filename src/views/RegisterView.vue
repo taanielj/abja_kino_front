3ae0347c3717e259e21bad5b0e3bdbc3a7da9466
TuuliRei <reiman.tuuli@gmail.com>
2023-06-01 14:27:20 +0300

    Tuuli: Register view

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,48 +1,51 @@
 <template>
-    <div class="container col col-4">
-        <div class="col text-center admin-table p-2">
-            <h1>Loo konto</h1>
-            <AlertModal
+    <div class="text-center">
+        <div class="row justify-content-center">
+            <div class="col col-3 mt-5 admin-table p-4">
+                <h1>Loo konto</h1>
+                <AlertModal
                     :message="errorMessage"
                     ref="alertModalRef"
-            />
-            <div @keydown.enter="login" class="row justify-content-center">
-                <div class="col col-6 mt-4">
-                    <div class="mb-3">
-                        <label for="username" class="form-label">Kasutajanimi</label>
-                        <input v-model="registrationRequest.username" type="text" class="form-control"
-                               id="username">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Parool</label>
-                        <input v-model="registrationRequest.password" type="password" class="form-control"
-                               id="password">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Parool uuesti</label>
-                        <input v-model="matchingPassword" type="password" class="form-control" id="password">
-                    </div>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">E-posti aadress</label>
-                        <input v-model="registrationRequest.email" type="email" class="form-control" id="password"
-                               placeholder="example@gmail.com">
-                    </div>
+                />
+                <div @keydown.enter="login" class="row justify-content-center">
+                    <div class="col col-6 mt-4">
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Kasutajanimi</label>
+                            <input v-model="registrationRequest.username" type="text" class="form-control"
+                                   id="username">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Parool</label>
+                            <input v-model="registrationRequest.password" type="password" class="form-control"
+                                   id="password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Parool uuesti</label>
+                            <input v-model="matchingPassword" type="password" class="form-control" id="password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">E-posti aadress</label>
+                            <input v-model="registrationRequest.email" type="email" class="form-control" id="password"
+                                   placeholder="example@gmail.com">
+                        </div>
 
-                </div>
-                <div class="row justify-content-center mt-5 mb-5">
-                    <div>
-                        <div class="col col-12">
-                            <button @click="navigateBack" type="submit"
-                                    class="btn button btn-outline-secondary custom-button me-3">Tagasi
-                            </button>
-                            <button @click="registerNewClient" type="submit"
-                                    class="btn button btn-outline-secondary custom-button me-3">Registreeri
-                            </button>
+                    </div>
+                    <div class="row justify-content-center mt-5 mb-5">
+                        <div>
+                            <div class="col col-12">
+                                <button @click="navigateBack" type="submit"
+                                        class="btn button btn-outline-secondary custom-button me-3">Tagasi
+                                </button>
+                                <button @click="registerNewClient" type="submit"
+                                        class="btn button btn-outline-secondary custom-button me-3">Registreeri
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 
